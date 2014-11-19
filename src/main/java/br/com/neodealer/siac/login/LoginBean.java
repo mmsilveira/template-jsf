70283d014fc8964ee55442635c3c2fa903b2718a
Moises M Silveira <msilveira.ti@gmail.com>
2014-11-19 00:25:27 -0200

    First commit

---
+++ src/main/java/br/com/neodealer/siac/login/LoginBean.java
@@ -0,0 +1,21 @@
+package br.com.neodealer.siac.login;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+
+@ManagedBean
+@ViewScoped
+public class LoginBean implements Serializable {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
